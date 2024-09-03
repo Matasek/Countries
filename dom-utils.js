03ab17796a3046303a9b6df3a873ebe745422d3d
Maciej Kryjom <maciejmatasek@gmail.com>
2024-09-03 21:48:18 +0200

    feat: add createInfoElement and createFlagImgElement functions

--- dom-utils.js
+++ dom-utils.js
@@ -1,7 +1,6 @@
-const createCountryItemElement = (country) => {
-  const countryElement = document.createElement("li");
-
-  return countryElement;
+export const renderCountriesList = (countries) => {
+  const rootElement = document.querySelector("#root");
+  rootElement.appendChild(createListElement(countries));
 };
 
 const createListElement = (countries) => {
@@ -13,7 +12,45 @@ const createListElement = (countries) => {
   return listElement;
 };
 
-export const renderCountriesList = (countries) => {
-  const rootElement = document.querySelector("#root");
-  rootElement.appendChild(createListElement(countries));
+const createCountryItemElement = (country) => {
+  const countryElement = document.createElement("li");
+
+  const countryNameElement = document.createElement("span");
+  countryNameElement.innerText = country.name;
+
+  countryElement.appendChild(createFlagImgElement(country));
+
+  countryElement.appendChild(countryNameElement);
+
+  countryElement.appendChild(
+    createInfoElement("Population", country.population)
+  );
+  countryElement.appendChild(createInfoElement("Region", country.region));
+  countryElement.appendChild(createInfoElement("Capital", country.capital));
+
+  return countryElement;
+};
+
+const createInfoElement = (labelName, value) => {
+  const infoElement = document.createElement("div");
+  const labelElement = document.createElement("strong");
+  labelElement.innerText = `${labelName}: `;
+  const valueElement = document.createElement("span");
+  valueElement.innerText = value;
+
+  infoElement.appendChild(labelElement);
+  infoElement.appendChild(valueElement);
+
+  return infoElement;
+};
+
+const createFlagImgElement = (country) => {
+  const imgContainerElement = document.createElement("div");
+  const imgElement = document.createElement("img");
+  imgElement.src = country.flagUrl;
+  imgElement.width = 160;
+  imgElement.height = 98;
+  imgContainerElement.appendChild(imgElement);
+
+  return imgContainerElement;
 };
